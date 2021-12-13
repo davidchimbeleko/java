df8fa7703ae1349912aa6f67902812ce185cf57f
Isaac Shukurani <imwakabira@ymail.com>
2021-12-13 14:25:25 +0200

    introduction to java

---
+++ Kaya.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Kaya{
+
+
+    public static void main(String[] args){
+
+
+ int length = 5;
+
+ int width = 6;
+
+int area = length * width ;
+
+ System.out.println (area);
+
+
+
+
+
+    }
+}
